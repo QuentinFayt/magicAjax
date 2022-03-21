6976cf14a061392b56c60d000b1ef39fa73e6000
QuentinFayt <quentin.fayt.p@gmail.com>
2022-03-21 01:22:40 +0100

    color and images

--- assets/script/Card.js
+++ assets/script/Card.js
@@ -1,36 +1,50 @@
 class Card {
-    constructor(name, cost=null, legendaryState=null, type, subType=null, effect, rarity, power=null, toughness=null) {
-        this.name = name;
-        this.cost = cost;
+    cardboard_color;
+
+    constructor(name, cost = null, legendaryState = null, type, subType = null, effect, rarity, power = null, toughness = null, color = null) {
+        this.name           = name;
+        this.cost           = cost;
         this.legendaryState = legendaryState;
-        this.type = type;
-        this.subType = subType;
-        this.effect = effect;
-        this.rarity = rarity;
-        this.power = power;
-        this.toughness = toughness;
+        this.type           = type;
+        this.subType        = subType;
+        this.effect         = effect;
+        this.rarity         = rarity;
+        this.power          = power;
+        this.toughness      = toughness;
+        this.color          = color;
+        this.checkColor();
     }
-    draw(){
+
+    checkColor() {
+        if (this.color.split("-").length > 1) {
+            this.cardboard_color = "mul";
+        }
+        else {
+            this.cardboard_color = "bl";
+        }
+    }
+
+    draw() {
         return `
-        <div>
-            <div class="nom">
+        <div class="card ${this.cardboard_color}">
+            <div class="nom ${this.cardboard_color}">
                 <p>${this.name}</p>
                 <p>${this.cost}</p>
             </div>
             <div class="img">
-               <img/>
+               <img src="./assets/images/${this.name}.jpg" alt="pic"/>
             </div>
-            <div class="type">`
-               +(this.legendaryState?"<p>Legendary</p>":"")+
+            <div class="type ${this.cardboard_color}">`
+               + (this.legendaryState ? "<p>Legendary</p>" : "") +
                `<p>${this.type}</p>`
-               +(this.subType?`<p>${this.subType}</p>`:"")+`
+               + (this.subType ? `<p>${this.subType}</p>` : "") + `
             </div>
-            <div class="text">
+            <div class="text ${this.cardboard_color}">
                 <p>${this.effect}</p>
             </div>
             <div class="rarity">
                 <p>${this.rarity}</p>`
-               +(this.type.toLowerCase()==="creature"?`<div><p>${this.power}</p><p>|</p><p>${this.toughness}</p>`:"")+`
+               + (this.type.toLowerCase() === "creature" ? `<div class="powtough ${this.cardboard_color}"><p>${this.power}</p><p>|</p><p>${this.toughness}</p>` : "") + `
                 </div>
             </div>
         </div>
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,27 @@
 document.addEventListener("keydown", (event) => {
     let key = event.key;
     if (key === "Enter") {
-      event.preventDefault();
-      let postValue = document.querySelector("#post").value;
-      let [id, name] = $("#post_id").is(":checked") ? [parseInt(postValue), ""] : [null, postValue];
-      let data = {id, name};
-       $("#post").val("");
-      if (data.id || data.name.length) {
-        $.post("http://magicajax-php/", data, (el) => {
-            let json = JSON.parse(el);
-            let card = new Card(json.card_name,json.card_cost,json.card_legendary_state,json.card_type,json.card_subtype,json.card_effect,json.card_rarity,json.card_power,json.card_toughness);
-            $("#result").empty().append(card.draw());
-        });
-      }else{
-           $("#result").empty().append("Cette ID n'existe pas!");
-      }
-      $("#post").focus();
+        event.preventDefault();
+        let postValue  = document.querySelector("#post").value;
+        let [id, name] = $("#post_id").is(":checked") ? [parseInt(postValue), ""] : [null, postValue];
+        let data       = {id, name};
+        $("#post").val("");
+        if (data.id || data.name.length) {
+            $.post("http://magicajax-php/", data, (el) => {
+                let json = JSON.parse(el);
+                let card = new Card(json.card_name, json.card_cost, json.card_legendary_state, json.card_type, json.card_subtype, json.card_effect, json.card_rarity, json.card_power, json.card_toughness, json.card_color);
+                $("#result").empty().append(card.draw());
+            });
+        }
+        else {
+            $("#result").empty().append("Cette ID n'existe pas!");
+        }
+        $("#post").focus();
     }
-   if(key === "ArrowLeft" || key === "ArrowRight"){
-       let check = $("#post_id").is(":checked")
-       $("#post_id").prop("checked",!(check));
-       $("#post_name").prop("checked",check);
-       $("#post").focus();
-   }
-  });
+    if (key === "ArrowLeft" || key === "ArrowRight") {
+        let check = $("#post_id").is(":checked")
+        $("#post_id").prop("checked", !(check));
+        $("#post_name").prop("checked", check);
+        $("#post").focus();
+    }
+});
