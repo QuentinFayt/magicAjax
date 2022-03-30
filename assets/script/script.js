1bf0d88b7fefcb4aae93bf7909ad7257eaaaf270
QuentinFayt <quentin.fayt.p@gmail.com>
2022-03-30 12:32:16 +0200

    correction

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,7 +5,7 @@ document.addEventListener("keydown", (event) => {
     let key = event.key;
     if (key === "Enter" && $("#post").is(":focus")) {
         event.preventDefault();
-        $("#result").css("overflow-y", "auto");
+        $("#result").css("overflow-y", "inherit");
         let postValue = document.querySelector("#post").value;
         let data      = {};
         if ($("#post_id").is(":checked")) {
@@ -21,6 +21,9 @@ document.addEventListener("keydown", (event) => {
                 if (!json.error) {
                     let card = new Card(json.card_name, json.card_cost, json.card_legendary_state, json.card_type, json.card_subtype, json.card_effect, json.card_rarity, json.card_power, json.card_toughness, json.card_color);
                     $("#result").empty().append(card.draw());
+                }
+                else {
+                    $("#result").empty().append("Cette ID n'existe pas!");
                 }
             });
         }
